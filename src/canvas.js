1b87a6fbd179c7ebdfec6c6b77cdea5dd68cb45b
Cb@ndy <cb@ndy@gmail.com>
2022-01-26 14:47:34 +0300

    cleanTrash

--- src/canvas.js
+++ src/canvas.js
@@ -1,14 +1,16 @@
 import style from './main.css';
-const { noise } = require('@chriscourses/perlin-noise');
+
 const htmlCanvas = document.getElementById('canvas');
 const canvas = htmlCanvas.getContext('2d');
 
 htmlCanvas.width = window.innerWidth - 4;
 htmlCanvas.height = window.innerHeight - 4;
+
 addEventListener('resize', () => {
     htmlCanvas.width = window.innerWidth - 4;
     htmlCanvas.height = window.innerHeight - 4;
 })
+
 var mouse = {
     x: htmlCanvas.width / 2,
     y: htmlCanvas.height / 2,
@@ -19,13 +21,11 @@ addEventListener('mousemove', (params) => {
     mouse.y = params.clientY;
 })
 
-
 class Circle {
     constructor(x, y, radius, color, offset) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.offset = offset;
         this.color = color;
 
     }
@@ -42,23 +42,9 @@ class Circle {
         this.draw();
     }
 }
-const circles = [];
 
-for (let i = 0; i < 100; i++) {
-    circles.push(new Circle(-30, -30, `25`, `hsl(${Math.random() * 360},50%,50%)`, i * 0.01));
-}
-
-let time = 0;
 function animate() {
-    canvas.fillStyle = 'rgba(0, 0, 0, 0.01)';
-    canvas.fillRect(0, 0, htmlCanvas.width, htmlCanvas.height);
-    circles.forEach((circle) => {
-        circle.update();
-        circle.x = noise(time + circle.offset + 20) * htmlCanvas.width;
-        circle.y = noise(time + circle.offset) * htmlCanvas.height;
-    });
-    time += 0.005;
     requestAnimationFrame(animate);
 }
-animate();
+// animate();
 
